c935a8d9bf88f4b4bb0c08a1b636350108daba35
Soufiane <formulacab@gmail.com>
2020-11-08 00:51:48 +0100

    index de l'application

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$routes = array(
+    "/\/(accueil)\/?(\d+)?/" => array('HomeController', 'index'),
+    '//' => array('HomeController', 'index')
+);
+
+// print("je suis dans l'index.php");
+// var_dump($routes);
+
+foreach ($routes as $url => $action) {
+  // var_dump($url);
+  // var_dump($_SERVER['REQUEST_URI']);
+  // var_dump($params);
+
+    $matches = preg_match($url, $_SERVER['REQUEST_URI'], $params);
+    // var_dump($matches);
+    // var_dump($params);
+
+    if ($matches > 0) {
+        // var_dump($action[0]);
+        if (isset($params[2])) {
+          $params = $params[2];
+        } else {
+          $params = 1;
+        }
+        $controller = new $action[0];
+        $controller->{$action[1]}($params);
+        break;
+    }
+}
