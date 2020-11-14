86dc65191065572860461b8c827f3b57b3082317
FlavGr <zencles75@gmail.com>
2020-11-14 19:18:40 +0100

    home page and footer css

--- views/home.php
+++ views/home.php
@@ -1,1 +1,19 @@
-<h1>Hello</h1>
+<div class="row home p-5">
+    <div class="col-md-6 home_presentation d-flex flex-column justify-content-center align-items-start">
+        <h1>Welcome to this Demo Ecommerce</h1>
+        <p class="mt-5">Ce projet à pour but de présenter une trame possible de développement PHP, Orienté Objet</p>
+        <a href="#descriptions" class="btn btn-green">Lire la suite</a>
+    </div>
+    <div class="col-md-6">
+        <img src="images\lala-azizli-tfNyTfJpKvc-unsplash.jpg" class="img-fluid rounded" alt="image who shows developers in work">
+        <span class="credit_image">Photo by <a href="https://unsplash.com/@lazizli?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Lala Azizli</a> on <a href="https://unsplash.com/s/photos/developers?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+    </div>
+    <div class="col-md-6 mx-auto" id="descriptions">
+        <h2 class="home_title">Qui a donc réalisé ce projet ?</h2>
+        <ul class="ml-5">
+            <li>Sofiane</li>
+            <li>Jonathan</li>
+            <li>Flavien</li>
+        </ul>
+    </div>
+</div>
