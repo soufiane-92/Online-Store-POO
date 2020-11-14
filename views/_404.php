55426531bdb6b5282c9252f884b1d1841c1498f0
FlavGr <zencles75@gmail.com>
2020-11-14 19:30:34 +0100

    css 404 page

--- views/_404.php
+++ views/_404.php
@@ -1,15 +1,13 @@
-<div class="row">
+<div class="row error_page">
         <div class="col-md-6 mx-auto mt-5">
             <div class="error-template">
-                <h1>
-                    Oops!</h1>
-                <h2>
-                    404 Not Found</h2>
+                <h1 class="block_green">Oops!</h1>
+                <h2>404 Not Found</h2>
                 <div class="error-details">
                     Sorry, an error has occured, Requested page not found!
                 </div>
                 <div class="error-actions">
-                    <a href="<?php Application::$root ?>home" class="btn btn-primary btn-lg btnError"><span class="glyphicon glyphicon-home"></span>
+                    <a href="<?php Application::$root ?>home" class="btn btn-green btn-lg btnError"><span class="glyphicon glyphicon-home"></span>
                         Take Me Home </a>
                 </div>
             </div>
