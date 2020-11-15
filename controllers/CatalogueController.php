e6c571dd3caa5bfcaa4bd05c1ce3d1bbc578f438
Soufiane <formulacab@gmail.com>
2020-11-15 13:43:58 +0100

    clean debug comment and old code

--- controllers/CatalogueController.php
+++ controllers/CatalogueController.php
@@ -29,19 +29,6 @@ class CatalogueController extends Controller
 
     public function categorie($value)
     {
-      // $url = $_SERVER['REQUEST_URI'];
-      // $tokens = explode('/', $url);
-      // $value = $tokens[sizeof($tokens)-1];
-      // $getPositionEqual = strpos($value, '=');
-      // if($getPositionEqual === false) {
-      //   return redirectErrorPage();
-      // } else {
-      //   $value = substr_replace($value, '', 0, $getPositionEqual + 1);
-      // }
-      // if(!verifUrl($value, ['Composition', 'Fleurs', 'Plantes'])) {
-      //   return redirectErrorPage();
-      // }
-
       $this->getModel('Categorie');
       // $categorie = new Categorie;
       if($this->Categorie->getOne("libelle",secureData($value, "input"))['id']){
--- controllers/PanierController.php
+++ controllers/PanierController.php
@@ -3,43 +3,6 @@ class PanierController extends Controller
 {
     public function index()
     {
-      // print("je suis dans la méthode index de la class HomeContriller");
-      // print_r($params);
-
-        // On instancie le model "Produit"
-        // $this->getModel('Produit');
-
-        // On creer le tableau de tous nos produits
-        // $produits = $this->Produit->getAll();
-        // $produit = $this->Produit->getOne("id","c03");
-
-// **************   test Ajouter dans Panier   ***************
-        // new Panier;
-        // var_dump($panier);
-// **************   test Ajouter dans Panier   ***************
-        // var_dump($_SESSION);
-        // print(Count(Panier::get()) === 0 ? "merde" : "fuck");
-
-        // Panier::add("c01", 1);
-        // Panier::add("c02", 1);
-        // Panier::add("c03", 2);
-        // Panier::add("c04", 1);
-        // Panier::add("c05", 4);
-        // Panier::add("c06", 1);
-// **************   test Supprimer element du Panier   ***************
-      // Panier::remove("c0wvxcv6");
-// **************   test Supprimer tout le Panier   ***************
-      // Panier::delete();
-// **************   recupere tout les produits du panier   ***************
-        // var_dump(Panier::show());
-// **************   recupere liste idProduit et quantité du panier   ***************
-        // var_dump(Panier::show());
-        var_dump(Session::get("auth"));
-        // var_dump($_SESSION['panier']);
-// ************************************************************************************
-
-
-
       if (isset($_POST["deleteProduit"]) && !empty($_POST["deleteProduit"])) {
         Panier::remove($_POST["deleteProduit"]);
       }
