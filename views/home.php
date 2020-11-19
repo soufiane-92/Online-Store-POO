476790db8206007969ac870fd3a4ac3179d85b21
FlavGr <zencles75@gmail.com>
2020-11-19 12:42:25 +0100

    home page improved - billing page created

--- views/home.php
+++ views/home.php
@@ -5,15 +5,36 @@
         <a href="#descriptions" class="btn btn-green">Lire la suite</a>
     </div>
     <div class="col-md-6">
-        <img src="<?= Application::$root ?>images\lala-azizli-tfNyTfJpKvc-unsplash.jpg" class="img-fluid rounded" alt="image who shows developers in work">
+        <img src="<?=Application::$root?>images\lala-azizli-tfNyTfJpKvc-unsplash.jpg" class="img-fluid rounded" alt="image who shows developers in work">
         <span class="credit_image">Photo by <a href="https://unsplash.com/@lazizli?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Lala Azizli</a> on <a href="https://unsplash.com/s/photos/developers?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
     </div>
-    <div class="col-md-6 mx-auto" id="descriptions">
-        <h2 class="home_title">Qui a donc réalisé ce projet ?</h2>
+</div>
+<div class="row home__infos">
+<div class="col-md-7 mx-auto" id="descriptions">
+        <h2 class="home_title">Qui a réalisé ce projet ?</h2>
         <ul class="ml-5">
             <li>Sofiane</li>
             <li>Jonathan</li>
             <li>Flavien</li>
         </ul>
+    </div>
+    <div class="col-md-7 mx-auto my-5" id="goal">
+        <h2 class="home_title">Pourquoi ?</h2>
+        <p>En commun accord avec nos enseignants de la partie informatique, dans le cadre du BTS SIO option SLAM, nous avons décidé de rafraichir un projet qui avait pour but de présenter l'architecture MVC.</p>
+        <p class="mt-2">Cette application ecommerce a été reconstruite en partant de zero en programmation object.</p>
+        <p>Pendant sa création, nous avons essayé de ne pas inutilement complexifier le code. Nous avons donc fait des choix spécifiques qui ne peuvent pas s'appliquer dans le cadre d'une application en production. </p>
+    </div>
+    <div class="col-md-7 mx-auto my-5" id="goal">
+        <h2 class="home_title">Important</h2>
+        <p class="mt-2">Cette application ne peut pas être utilisé en production, notamment pour des raisons de sécurité.</p>
+
+    </div>
+</div>
+<div class="row home__doc">
+    <div class="col-md-7 mx-auto" id="descriptions">
+        <h2 class="home_title">Présentation graphique avec MCD</h2>
+    </div>
+    <div class="col-md-10 mx-auto" id="descriptions">
+        <img class="img-fluid" src="<?=Application::$root?>images/mcd.png" alt="description mcd">
     </div>
 </div>
--- views/layout/templates/categories.php
+++ views/layout/templates/categories.php
@@ -3,24 +3,23 @@
   <h1 class="ml-5 my-4">Catégories</h1>
   <div class="col-12 p-5">
     <?php
-    $categories = new Categorie;
-    $allCategories = $categories->getAll() ?? [];
-// var_dump($allCategories);
-    ?>
+$categories = new Categorie;
+$allCategories = $categories->getAll() ?? [];
+?>
     <ul class="list-group list-categories">
-      <?php foreach($allCategories as $categorie): ?>
-        <li class="list-group-item"><a href="<?= Application::$root . 'catalogue/' . strtolower($categorie['libelle']) ?>" ><?= $categorie['libelle'] ?></a></li>
-      <?php  endforeach ?>
+      <?php foreach ($allCategories as $categorie): ?>
+        <li class="list-group-item"><a href="<?=Application::$root . 'catalogue/' . strtolower($categorie['libelle'])?>" ><?=$categorie['libelle']?></a></li>
+      <?php endforeach?>
     </ul>
   </div>
 </div>
 <?php
 if (Panier::size() > 0) {
-?>
+    ?>
 <form class=""  method="post">
   <input type="hidden" name="deletePanier" value="ok">
   <button type="submit" name="deletePanierBtn" class="btn btn-lg btn-danger">
     Vider Panier
   </button>
 </form>
-<?php } ?>
+<?php }?>
